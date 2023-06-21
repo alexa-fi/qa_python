d34fc2ba3133e78574d2cc43f16ae49d122fdc19
Aleksandra Fisher <5957099@gmail.com>
2023-06-21 11:10:42 +0400

    Added 8 tests in tests.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # qa_python
-test_add_new_book_add_two_books - Добавляет две новые книги 
+test_add_new_book_add_two_books - Добавляет две новые книги
 test_add_same_book_twice_false - Проверяет, что нельзя добавить одну и ту же книгу дважды 
 test_set_rating_books_from_list_false - Проверяет, что нельзя выставить рейтинг книге которой нет в списке 
 test_set_rating_less_then_one_false - Проверяет, что нельзя выставить рейтинг книге меньше 1 
@@ -7,4 +7,4 @@ test_set_rating_more_then_ten_false - Проверяет, что нельзя в
 test_get_book_not_in_list_false - Проверяет, что у не добавленной книги нет рейтинга 
 test_add_book_favorites_true - добавляет книгу в избранное 
 test_add_book_in_favorites_which_not_in_dict_false - проверяет что нельзя добавить книгу в избранное, если ее нет в словаре 
-test_delete_book_from_favorites - проверяет удаление книги из избранного 
+test_delete_book_from_favorites - проверяет удаление книги из избранного
--- tests.py
+++ tests.py
@@ -23,55 +23,72 @@ class TestBooksCollector:
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
-    # Нельзя добавить одну и ту же книгу дважды
+    """Нельзя добавить одну и ту же книгу дважды"""
     def test_add_same_book_twice_false(self):
         collector = BooksCollector()
         collector.add_new_book('Сказки Пушкина')
         collector.add_new_book('Сказки Пушкина')
         assert len(collector.get_books_rating()) == 1
 
-    # Нельзя выставить рейтинг книге, которой нет в списке
+    """Нельзя выставить рейтинг книге, которой нет в списке"""
     def test_set_rating_books_from_list_false(self):
         collector = BooksCollector()
         collector.set_book_rating('Как заработать миллион проблем', 6)
-        assert collector.books_rating == {}
+        assert collector.get_books_rating() == {}
 
-    # Нельзя выставить рейтинг меньше 1
+    """Нельзя выставить рейтинг меньше 1"""
     def test_set_rating_less_then_one_false(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 0)
         assert collector.get_books_rating() == {'Гордость и предубеждение и зомби': 1}
 
-    # Нельзя выставить рейтинг больше 10
+    """ Нельзя выставить рейтинг больше 10"""
     def test_set_rating_more_then_ten_false(self):
         collector = BooksCollector()
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_rating('Что делать, если ваш кот хочет вас убить', 11)
         assert collector.get_books_rating() == {'Что делать, если ваш кот хочет вас убить': 1}
 
-    # У не добавленной книги нет рейтинга.
+    """У не добавленной книги нет рейтинга"""
     def test_get_book_not_in_list_false(self):
         collector = BooksCollector()
         assert collector.get_book_rating('Преступление и наказание') is None
 
-    # Добавление книги в избранное
+    """ Добавление книги в избранное"""
     def test_add_book_favorites_true(self):
         collector = BooksCollector()
         collector.add_new_book('Атлант расправил плечи')
         collector.add_book_in_favorites('Атлант расправил плечи')
         assert collector.favorites == ['Атлант расправил плечи']
 
-    # Нельзя добавить книгу в избранное, если её нет в словаре books_rating
+    """Нельзя добавить книгу в избранное, если её нет в словаре books_rating"""
     def test_add_book_in_favorites_which_not_in_dict_false(self):
         collector = BooksCollector()
         collector.add_book_in_favorites('Мастер и Маргарита')
         assert collector.favorites == []
 
-    # Проверка удаления книги из избранного
+    """ Проверка удаления книги из избранного"""
     def test_delete_book_from_favorites(self):
         collector = BooksCollector()
         collector.add_new_book('Aтлант расправил плечи')
         collector.add_book_in_favorites('Атлант расправил плечи')
         collector.delete_book_from_favorites('Атлант расправил плечи')
         assert collector.favorites == []
+
+    """Вывод списка книг со специфическим рейтингом"""
+    def test_get_books_with_specific_rating_true(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+
+        collector.set_book_rating('Простоквашино', 9)
+
+        collector.add_new_book('Евгений Онегин')
+
+        collector.add_new_book('Котенок Шмяк')
+
+        collector.set_book_rating('Котенок Шмяк', 10)
+
+        assert collector.get_books_with_specific_rating(9) == ['Простоквашино']
+
